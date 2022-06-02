38e3a68359ac16a8cd703fdb12e1d551273a282c
muhammedessamq <94935993+muhammedessamq@users.noreply.github.com>
2022-06-02 23:54:05 +0200

    Final version

---
+++ LastVersion/Busejb/ejbModule/EJBs/User.java
@@ -0,0 +1,81 @@
+package EJBs;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Typed;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Stateless
+@LocalBean
+@Entity
+@Table(name="USER")
+
+
+public class User {
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id ;
+	@Size (min=2,max=15)
+	private String username ;
+	
+	
+	@Size (min=2,max=50)
+	private String full_name ;
+	
+	private String password ;
+	private String role ;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFull_name() {
+		return full_name;
+	}
+
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	public String sayHello() {
+		return "Hello";
+	}
+	
+	
+}
+
